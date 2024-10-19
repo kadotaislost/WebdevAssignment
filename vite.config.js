10a1de51beb70303db16d8e66830d3bf9f4db0d5
Sachin Lama <sachin2211068@iimscollege.edu.np>
2024-10-19 16:26:02 +0545

    configuring vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
         page1: resolve(__dirname, "self-assessment/index.html"), // Custom index.html in folder1
         page2: resolve(__dirname, "self-assessment/test/index.html"), // Custom index.html in folder1
         page3: resolve(__dirname, "resource-library/index.html"), // Custom index.html in folder2
+        testJs: resolve(__dirname, "self-assessment/test/test.js"), // Add this line
+        resourceJs: resolve(__dirname, "resource-library/resource.js"), // Add this line
       },
     },
   },
